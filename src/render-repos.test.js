b7897d03b5e72fa410a57c164d8cd8d2e1330764
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-07-28 00:26:07 +0200

    working on render-repos component

--- src/render-repos.test.js
+++ src/render-repos.test.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Enzyme, { configure, shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import RenderRepos from './render-repos';
 
 configure({adapter: new Adapter()});
+
+//aqui mi proo es una string
